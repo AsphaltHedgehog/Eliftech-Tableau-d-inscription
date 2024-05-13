e71c876f90e5e7fad9f868cbdfecf86acc2a6fb1
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-05-14 00:27:52 +0300

    completed routes
    added routes, layout and all needed pages

--- front-testWork/src/App.tsx
+++ front-testWork/src/App.tsx
@@ -1,7 +1,23 @@
+import { Route, Routes } from "react-router";
+
+// components
+import Layout from "@/components/layout/Layout";
+
+// pages
+import HomePage from "./pages/homePage/HomePage";
+import RegistrationPage from "./pages/registrationPage/RegistrationPage";
+import ParticipantsPage from "./pages/participantsPage/ParticipantsPage";
 
 function App() {
   return (
     <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="registration/:id" element={<RegistrationPage />} />
+          <Route path="participants/:id" element={<ParticipantsPage />} />
+        </Route>
+      </Routes>
     </>
   );
 }
