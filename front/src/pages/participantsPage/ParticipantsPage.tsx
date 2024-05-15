261f3350efc1434a6efbb7b3700dc413bc8ea3fc
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-05-15 23:35:40 +0300

    complete search

--- front/src/pages/participantsPage/ParticipantsPage.tsx
+++ front/src/pages/participantsPage/ParticipantsPage.tsx
@@ -1,18 +1,39 @@
 import { useGetParticipantsQuery } from "@/redux/eventsApi/operations";
+import { useState } from "react";
 import { useParams } from "react-router";
 
 const ParticipantsPage = () => {
   const { id } = useParams<{ id: string }>();
+  const [searchQuery, setSearchQuery] = useState("");
 
   const { data: participants, isLoading } = useGetParticipantsQuery(id ? id : "");
+
+  const filteredParticipants = participants?.participants.filter((participant) => {
+    const nameMatch = participant.name.toLowerCase().includes(searchQuery.toLowerCase().trim());
+    const emailMatch = participant.email.toLowerCase().includes(searchQuery.toLowerCase().trim());
+    return nameMatch || emailMatch;
+  });
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
 
   return (
     <main className="container mx-auto px-4">
       <h2 className="mb-4 text-3xl font-semibold">Participants</h2>
+      <div className="mb-4">
+        <input
+          type="text"
+          placeholder="Search by name or email"
+          value={searchQuery}
+          onChange={handleSearchChange}
+          className="rounded-md border border-gray-300 px-4 py-2 focus:border-blue-400 focus:outline-none"
+        />
+      </div>
       {isLoading ? <p>Loading...</p> : null}
-      {participants && participants.participants.length > 0 && (
+      {filteredParticipants && filteredParticipants.length > 0 && (
         <ul className="grid grid-cols-2 gap-4">
-          {participants.participants.map((participant) => (
+          {filteredParticipants.map((participant) => (
             <li key={participant._id} className="rounded bg-gray-100 p-4">
               <h3 className="mb-2 text-xl font-semibold">{participant.name}</h3>
               <p className="mb-2">{participant.email}</p>
@@ -20,9 +41,10 @@ const ParticipantsPage = () => {
           ))}
         </ul>
       )}
-      {participants && participants.participants.length === 0 && !isLoading && (
-        <p className="mb-2 text-xl font-semibold">No participants found.</p>
-      )}
+      {!filteredParticipants ||
+        (filteredParticipants.length === 0 && !isLoading && (
+          <p className="mb-2 text-xl font-semibold">No participants found.</p>
+        ))}
     </main>
   );
 };
