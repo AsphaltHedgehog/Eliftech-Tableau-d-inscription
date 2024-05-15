854a2a291f8c55fd111fa6836f5ebce1850f7918
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-05-15 16:11:49 +0300

    completed backend

--- back/src/controllers/events.ts
+++ back/src/controllers/events.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import HttpError from "../helpers/HttpError";
 import ctrlWrapper from "../decorators/ctrlWrapper";
 import Event from "../models/Event";
+import Participant, { IParticipantDatabaseResponse } from "../models/Participant";
 
 const getAll = async (req: Request, res: Response): Promise<void> => {
   const { page, pageSize } = req.query;
@@ -18,16 +19,57 @@ const getAll = async (req: Request, res: Response): Promise<void> => {
 
     res.status(200).json({ data: result, totalEvents: totalEventsCount });
   } catch (error: any) {
-    throw HttpError(400, "Internal server/database error");
+    throw HttpError(500, "Internal server/database error");
   }
 };
 
 const register = async (req: Request, res: Response): Promise<void> => {
-  res.status(201).json({});
+  const { event, email, name } = req.body;
+
+  const eventInfo = await Event.findById(event);
+  if (!eventInfo) {
+    throw HttpError(404, "Event not found");
+  }
+
+  const participant = await Participant.findOne({ event, email });
+  if (participant) {
+    throw HttpError(409, "Participant already registered for this event");
+  }
+
+  try {
+    const newParticipant = await Participant.create({ ...req.body });
+    if (!newParticipant) {
+      throw HttpError(404, "Failed to create participant");
+    }
+
+    const typedNewParticipant = newParticipant as unknown as IParticipantDatabaseResponse;
+
+    res.status(201).json({
+      event: typedNewParticipant.event,
+      name: typedNewParticipant.name,
+      email: typedNewParticipant.email
+    });
+  } catch (error) {
+    throw HttpError(500, "Internal server/database error");
+  }
 };
 
 const participants = async (req: Request, res: Response): Promise<void> => {
-  res.status(200).json({});
+  const { event } = req.body;
+
+  const eventInfo = await Event.findById(event);
+
+  if (!eventInfo) {
+    throw HttpError(404, "Event not found");
+  }
+
+  try {
+    const participants = await Participant.find({ event });
+
+    res.status(200).json({ participants });
+  } catch (error) {
+    throw HttpError(500, "Internal server/database error");
+  }
 };
 
 export default {
