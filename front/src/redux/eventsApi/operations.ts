90d262ab5486f0f5c9411b065334868784fc70fc
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-05-15 17:48:48 +0300

    completed api

--- front/src/redux/eventsApi/operations.ts
+++ front/src/redux/eventsApi/operations.ts
@@ -1,11 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { IEvents, IGetEventsResponse } from "../types";
+import { IEvents, IGetEventsResponse, IGetParticipantsResponse, IParticipant, ISetParticipantResponse } from "../types";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
   reducerPath: "main",
   tagTypes: ["Events"],
   endpoints: (build) => ({
+    // get all events
     getEvents: build.query<IGetEventsResponse<IEvents>, { page?: number; pageSize?: number }>({
       query: ({ page = 1, pageSize = 6 }) => `api/events/?page=${page}&pageSize=${pageSize}`,
       serializeQueryArgs: ({ endpointName }) => {
@@ -27,9 +28,34 @@ export const api = createApi({
       },
       forceRefetch({ currentArg, previousArg }) {
         return currentArg !== previousArg;
-      }
+      },
+      keepUnusedDataFor: 120
+    }),
+    setParticipant: build.query<
+      ISetParticipantResponse,
+      { id: string; name: string; email: string; dateOfBirth: string; heardAbout: string }
+    >({
+      query: ({ id, name, email, dateOfBirth, heardAbout }) => ({
+        url: `api/events/registration`,
+        body: {
+          event: id,
+          name,
+          email,
+          dateOfBirth,
+          heardAbout
+        }
+      }),
+      keepUnusedDataFor: 1
+    }),
+    // get all participants
+    getParticipants: build.query<IGetParticipantsResponse<IParticipant>, string>({
+      query: (id: string) => ({
+        url: `api/events/participants`,
+        body: { event: id }
+      }),
+      keepUnusedDataFor: 120
     })
   })
 });
 
-export const { useGetEventsQuery } = api;
+export const { useGetEventsQuery, useGetParticipantsQuery, useSetParticipantQuery } = api;
