57f8bb7a1f2ed7e0a5b1487ec5cb651bb954ad55
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2024-05-15 23:02:36 +0300

    completed participants page

--- front/src/pages/participantsPage/ParticipantsPage.tsx
+++ front/src/pages/participantsPage/ParticipantsPage.tsx
@@ -1,5 +1,30 @@
+import { useGetParticipantsQuery } from "@/redux/eventsApi/operations";
+import { useParams } from "react-router";
+
 const ParticipantsPage = () => {
-  return <div>ParticipantsPage</div>;
+  const { id } = useParams<{ id: string }>();
+
+  const { data: participants, isLoading } = useGetParticipantsQuery(id ? id : "");
+
+  return (
+    <main className="container mx-auto px-4">
+      <h2 className="mb-4 text-3xl font-semibold">Participants</h2>
+      {isLoading ? <p>Loading...</p> : null}
+      {participants && participants.participants.length > 0 && (
+        <ul className="grid grid-cols-2 gap-4">
+          {participants.participants.map((participant) => (
+            <li key={participant._id} className="rounded bg-gray-100 p-4">
+              <h3 className="mb-2 text-xl font-semibold">{participant.name}</h3>
+              <p className="mb-2">{participant.email}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+      {participants && participants.participants.length === 0 && !isLoading && (
+        <p className="mb-2 text-xl font-semibold">No participants found.</p>
+      )}
+    </main>
+  );
 };
 
 export default ParticipantsPage;
